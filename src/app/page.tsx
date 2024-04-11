8a2d7e94eeefd9af86c976e78b8ff33aeda15833
witchbl4des <menzo.smit-adam@epitech.eu>
2024-04-12 00:01:07 +0200

    Update page layout and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import background from "../../public/background.jpg"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image src={background} alt="bgimage" className="w-full h-full object-cover object-center opacity-25" />
     </main>
   );
 }
