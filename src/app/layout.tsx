88ee0188ad396bcdcb8971d9d8a08302f64d8d57
witchbl4des <menzo.smit-adam@epitech.eu>
2024-04-18 19:24:26 +0200

    Refactor layout component and remove sparklesBackground

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import SparklesBackground from "@/components/about/sparklesBackground";
+import SparklesBackground from "@/components/sparkles/sparklesBackground";
+import Navbar from "@/components/navbar/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
+        <Navbar />
         <SparklesBackground />
         {children}</body>
     </html>
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-slate-300/30 bg-[#03001417] backdrop-blur-md z-50 px-10">
+
+        </div>
+    )
+}
+
+export default Navbar;
